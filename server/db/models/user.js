994bdffa1f7352c14081d92dca9ad4ffcf73bdfa
kookiedoodle5 <parin.a.gandhi@gmail.com>
2022-01-05 00:04:50 -0500

    Added router files

---
+++ server/db/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowerCase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email must be valid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+    trim: true,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("The password cannot contain 'password'");
+      }
+    },
+  },
+});
+
+module.exports = User;
